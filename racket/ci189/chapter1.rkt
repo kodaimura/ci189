e94a28935918da743395850eb4acd9200203a8fa
kodaimura <kodaimura123@gmail.com>
2023-08-09 10:03:46 +0900

    add racket 1.4

--- racket/ci189/chapter1.rkt
+++ racket/ci189/chapter1.rkt
@@ -24,10 +24,6 @@
 (permutations? "abcdab" "aabdcb")
 
 ;1.3
-(define replace-space-with%20
-  (lambda (s n)
-    (replace (substring s 0 n) " " "%20")))
-
 (define replace
   (lambda (s from to)
     (let [[x (string-length from)]]
@@ -38,8 +34,29 @@
           ((string=? (substring s 0 x) from) (loop (string-append ret to) (substring s x)))
           (else (loop (string-append ret (substring s 0 1)) (substring s 1))))))))
 
+(define replace-space-with%20
+  (lambda (s n)
+    (replace (substring s 0 n) " " "%20")))
+
 (replace"a b c" " " "%20")
 (replace"abcabc" "ab" "%20")
 (replace-space-with%20 "a b c" 4)
 (replace-space-with%20 "a b c   " 6)
 
+;1.4
+(define palindrome?
+  (lambda (s)
+    (let [[s (replace s " " "")]]
+      (string=? s (list->string (reverse (string->list s)))))))
+
+(define palindrome-permutations?
+  (lambda (s1 s2)
+    (and (palindrome? s1)
+         (palindrome? s2)
+         (permutations? s1 s2))))
+
+(palindrome? "abc dba")
+(palindrome? "abc cba")
+(palindrome? "ab c cba")
+(palindrome-permutations? "taco cat" "atco ota")
+(palindrome-permutations? "taco cat" "atco cta")
