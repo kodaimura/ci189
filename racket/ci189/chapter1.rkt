ea369dbf601debaac00988d616d63b866aaaf4a0
kodaimura <kodaimura123@gmail.com>
2023-08-15 23:35:11 +0900

    add racket 1.5

--- racket/ci189/chapter1.rkt
+++ racket/ci189/chapter1.rkt
@@ -60,3 +60,45 @@
 (palindrome? "ab c cba")
 (palindrome-permutations? "taco cat" "atco ota")
 (palindrome-permutations? "taco cat" "atco cta")
+
+;1.5
+;(string-length s1) = (string-length s1) の場合
+(define can-convert-one-shot-replace?
+  (lambda (s1 s2)
+    (let loop [[count 0] [s1 s1] [s2 s2]]
+      (cond
+        ((> count 1) #f)
+        ((string=? s1 "") #t)
+        ((string=? (substring s1 0 1) (substring s2 0 1))
+         (loop count (substring s1 1) (substring s2 1)))
+        (else (loop (+ count 1) (substring s1 1) (substring s2 1)))))))
+
+;(string-length s1) > (string-length s1) の場合
+(define can-convert-one-shot-delete?
+  (lambda (s1 s2)
+    (cond
+      ((string=? s2 "") (= 1 (string-length s1)))
+      ((string=? (substring s1 0 1) (substring s2 0 1))
+       (can-convert-one-shot-delete? (substring s1 1) (substring s2 1)))
+      (else (string=? (substring s1 1) s2)))))
+
+;(string-length s1) < (string-length s1) の場合
+(define can-convert-one-shot-insert?
+  (lambda (s1 s2)
+    (can-convert-one-shot-delete? s2 s1)))
+
+(define can-convert-one-shot?
+  (lambda (s1 s2)
+    (let [[s1l (string-length s1)]
+          [s2l (string-length s2)]]
+      (cond
+        ((string=? s1 s2) #t)
+        ((= s1l s2l) (can-convert-one-shot-replace? s1 s2))
+        ((> s1l s2l) (can-convert-one-shot-delete? s1 s2))
+        (else (can-convert-one-shot-insert? s1 s2))))))
+
+(can-convert-one-shot? "pale" "pale")
+(can-convert-one-shot? "pale" "ple")
+(can-convert-one-shot? "pales" "pale")
+(can-convert-one-shot? "pale" "bale")
+(can-convert-one-shot? "pale" "bake")
